4d4351c4899468f3a544b999bc1cc1a588a92c15
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-16 15:15:16 +0900

    Update __init__.py

--- applicability_domain/__init__.py
+++ applicability_domain/__init__.py
@@ -1,1 +1,16 @@
+# 正規表現でtextファイルと同じやり方で読み込むのですべて手打ちする．
+
 from .applicability_domain import *
+
+__version__      = '0.1.0'
+__license__      = 'Apache License 2.0'
+__author__       = 'yu9824'
+__copyright__    = 'Copyright © 2021 yu9824'
+__author_email__ = 'yu.9824.job@gmail.com'
+__user_id__ = 'yu9824'
+__app_name__ = 'template'
+__url__ = 'http://github.com/yu9824/applicability_domain/'
+
+__all__ = [
+    'ApplicabilityDomainDetector',
+    ]
